e012fd6ae976f8bcbc6d487c273182e63d9af620
Chu Shao <cshao@churenshao.com>
2014-03-29 00:02:53 -0700

    (Slide 09) returning data - backend done

--- routes/hashtag.js
+++ routes/hashtag.js
@@ -1,12 +1,23 @@
 var auth = require('../auth');
 
 exports.getHashtag = function(req, res) {
-	//test to see if it works
-	auth.ig.tags.info({
-	name: 'sushi',
+	auth.ig.tags.recent({
+	name: req.body.hashtag,
 	complete: function(data) {
-		console.log(data);
+		//create an image array
+		imageArr = [];
+		//Map will iterate through the returned data obj
+		data.map(function(item) {
+			//create temporary json object
+			tempJSON = {};
+			tempJSON.url = item.images.standard_resolution.url;
+			//insert json object into image array
+			imageArr.push(tempJSON);
+		});
+		//turn image array and the hashtag name into data to return
+		data = {imageArray: imageArr, hashtagValue:req.body.hashtag};
+		//return data to the webpage
+		res.render('hashtag', data);
 	}
-});
-	res.render('hashtag');
+	});
 }
