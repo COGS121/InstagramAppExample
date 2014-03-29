8d120352f65000316edeac476b6e60377e2b723a
Chu Shao <cshao@churenshao.com>
2014-03-28 20:47:51 -0700

    (Slide 05) initial set up of environment variables for instagram

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-
+//dependencies for each module used
 var express = require('express');
 var http = require('http');
 var handlebars = require('express3-handlebars');
 var app = express();
 
+//load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
+
+//add instagram api 
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 
+//routes
 app.get('/', function (req, res) {
 	res.render('index');
 });
 
+//set environment ports and start application
 app.set('port', process.env.PORT || 3000);
-
-
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
